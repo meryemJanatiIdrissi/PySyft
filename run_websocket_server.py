a5651097abfcbc3fe6d6dd0019a993bd6d05e8c9
meryemJanatiIdrissi <meryem.janati@um6p.ma>
2020-03-26 03:24:56 +0100

    Initial Commit

--- run_websocket_server.py
+++ run_websocket_server.py
@@ -1,108 +1,108 @@
-from multiprocessing import Process
-import argparse
-import os
 import logging
-import syft as sy
-from syft.workers.websocket_server import WebsocketServerWorker
-import torch
+import argparse
 import numpy as np
+import torch
 from torchvision import datasets
 from torchvision import transforms
 
-
-KEEP_LABELS_DICT = {
-    "alice": [0, 1, 2, 3],
-    "bob": [4, 5, 6],
-    "charlie": [7, 8, 9],
-    "testing": list(range(10)),
-    None: list(range(10)),
-}
+import syft as sy
+from syft.workers import websocket_server
+from random import sample 
+import pickle
 
 
-def start_websocket_server_worker(id, host, port, hook, verbose, keep_labels=None, training=True):
+
+    
+def start_websocket_server_worker(id, host, port, hook, verbose, dataset, training=True):
     """Helper function for spinning up a websocket server and setting up the local datasets."""
-
-    server = WebsocketServerWorker(id=id, host=host, port=port, hook=hook, verbose=verbose)
-
-    # Setup toy data (mnist example)
-    mnist_dataset = datasets.MNIST(
-        root="./data",
-        train=training,
-        download=True,
-        transform=transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
-        ),
+    
+    server = websocket_server.WebsocketServerWorker(
+        id=id, host=host, port=port, hook=hook, verbose=verbose
     )
+    
+    
+    # --------------- Setup toy data ----------------#
+    
+    transform = transforms.Compose([transforms.ToTensor(),
+                               transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))])
+    
+    if(dataset == "fashionmnist"):
+        toy_dataset = datasets.FashionMNIST('./data', train = training, download=True,
+            transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+            ]),)
 
+        dataset_key = "fashionmnist"
+    
+    if(dataset == "mnist"):
+        toy_dataset = datasets.MNIST(root="./data", train=training, download=True,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))
+                ]),)    
+        dataset_key = "mnist"
+        
+    if(dataset == "cifar10"):
+        toy_dataset = datasets.CIFAR10(root="./data", train=training, download=True,
+                   transform= transform)
+        dataset_key = "cifar10"
+
+        
+    
     if training:
-        indices = np.isin(mnist_dataset.targets, keep_labels).astype("uint8")
-        logger.info("number of true indices: %s", indices.sum())
-        selected_data = (
-            torch.native_masked_select(mnist_dataset.data.transpose(0, 2), torch.tensor(indices))
+        with open("./split/%d" % int(id), "rb") as fp:   # Unpickling
+            split = pickle.load(fp)      
+        indices = np.isin([i for i in range(len(toy_dataset.data))], split)
+        logger.info("Number of true indices for client %s is %s : ", id, indices.sum())
+        
+        
+        if(dataset == "mnist" or dataset == "fashionmnist"):
+            selected_data = (
+            torch.native_masked_select(toy_dataset.data.transpose(0, 2), torch.tensor(indices))
             .view(28, 28, -1)
             .transpose(2, 0)
-        )
-        logger.info("after selection: %s", selected_data.shape)
-        selected_targets = torch.native_masked_select(mnist_dataset.targets, torch.tensor(indices))
+            )
+        
+        if(dataset == "cifar10"):
+            selected_data = (
+                torch.native_masked_select(torch.tensor(toy_dataset.data.transpose((3,1,2,0))), torch.tensor(indices))
+                .view(3, 32, 32, -1)
+                .transpose(3, 0)
+            )
 
+        logger.info("after selection: %s", selected_data.shape)
+        selected_targets = torch.native_masked_select(torch.tensor(toy_dataset.targets), torch.tensor(indices))
         dataset = sy.BaseDataset(
-            data=selected_data, targets=selected_targets, transform=mnist_dataset.transform
+            data=selected_data, targets=selected_targets, transform=toy_dataset.transform
         )
-        key = "mnist"
+        key = dataset_key
     else:
         dataset = sy.BaseDataset(
-            data=mnist_dataset.data,
-            targets=mnist_dataset.targets,
-            transform=mnist_dataset.transform,
+            data=torch.tensor(toy_dataset.data),
+            targets=torch.tensor(toy_dataset.targets),
+            transform=toy_dataset.transform,
         )
-        key = "mnist_testing"
+        key = dataset_key+ "_testing"
 
+        
     server.add_dataset(dataset, key=key)
     count = [0] * 10
     logger.info(
-        "MNIST dataset (%s set), available numbers on %s: ", "train" if training else "test", id
+        "CIFAR dataset (%s set), available numbers on %s: ", "train" if training else "test", id
     )
     for i in range(10):
         count[i] = (dataset.targets == i).sum().item()
         logger.info("      %s: %s", i, count[i])
-
+    
     logger.info("datasets: %s", server.datasets)
     if training:
-        logger.info("len(datasets[mnist]): %s", len(server.datasets[key]))
+        logger.info("len(datasets): %s", len(server.datasets[key]))
 
     server.start()
     return server
 
 
-def start_proc(participant, kwargs):  # pragma: no cover
-    """ helper function for spinning up a websocket participant """
-
-    def target():
-        server = participant(**kwargs)
-        server.start()
-
-    p = Process(target=target)
-    p.start()
-    return p
-
-
-def start_proc_steal_data_over_sockets(participant, kwargs):  # pragma: no cover
-    """ helper function for spinning up a websocket participant """
-
-    def target():
-        server = participant(**kwargs)
-        private_data = torch.tensor([1, 1, 1, 1, 1])
-        private_data.private = True
-        server._objects[1] = private_data
-        server.start()
-
-    p = Process(target=target)
-    p.start()
-    return p
-
-
 if __name__ == "__main__":
-
     # Logging setup
     FORMAT = "%(asctime)s | %(message)s"
     logging.basicConfig(format=FORMAT)
@@ -130,45 +130,20 @@ if __name__ == "__main__":
         "--verbose",
         "-v",
         action="store_true",
-        help="""if set, websocket server worker will be started in verbose mode""",
+        help="if set, websocket server worker will be started in verbose mode",
     )
-    parser.add_argument(
-        "--notebook",
-        type=str,
-        default="normal",
-        help="""can run websocket server for websockets examples of mnist/mnist-parallel or
-        pen_testing/steal_data_over_sockets. Type 'mnist' for starting server
-        for websockets-example-MNIST, `mnist-parallel` for websockets-example-MNIST-parallel
-        and 'steal_data' for pen_tesing stealing data over sockets""",
-    )
-    args = parser.parse_args()
+    parser.add_argument("--dataset", help="Dataset held by users")
 
+    args = parser.parse_args()
     # Hook and start server
     hook = sy.TorchHook(torch)
-
-    # server = start_proc(WebsocketServerWorker, kwargs)
-    if args.notebook == "normal" or args.notebook == "mnist" or args.notebook == "steal_data":
-        kwargs = {
-            "id": args.id,
-            "host": args.host,
-            "port": args.port,
-            "hook": hook,
-            "verbose": args.verbose,
-        }
-        if os.name != "nt" and (args.notebook == "normal" or args.notebook == "mnist"):
-            server = start_proc(WebsocketServerWorker, kwargs)
-        elif os.name != "nt" and args.notebook == "steal_data":
-            server = start_proc_steal_data_over_sockets(WebsocketServerWorker, kwargs)
-        else:
-            server = WebsocketServerWorker(**kwargs)
-            server.start()
-    elif args.notebook == "mnist-parallel":
-        server = start_websocket_server_worker(
-            id=args.id,
-            host=args.host,
-            port=args.port,
-            hook=hook,
-            verbose=args.verbose,
-            keep_labels=KEEP_LABELS_DICT[args.id],
-            training=not args.testing,
-        )
+    server = start_websocket_server_worker(
+        id=args.id,
+        host=args.host,
+        port=args.port,
+        hook=hook,
+        verbose=args.verbose,
+        dataset=args.dataset,
+        training=not args.testing,
+    )
+    
